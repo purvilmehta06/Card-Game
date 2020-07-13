e1557eec0ccc3ea3e33c48177adee014e147d789
Purvil Mehta <purvilmehta06@gmail.com>
2020-07-13 12:38:26 +0530

    add script

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ function shuffle(array) {
 }
 
 //Server Start
-let PORT = 5000
+let PORT = process.env.PORT || 5000
 server.listen(PORT, () => {
   console.log(
     `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}...`
